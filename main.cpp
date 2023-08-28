b42c4366d33221d9641098da88c5b25b30ed225a
Sukhvinder Singh <sukhe353@gmail.com>
2023-08-28 19:30:03 +0530

    feat: created first cpp program

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Multiply two number
+    // int num1;
+    // int num2;
+    // cout << "Input number 1\n";
+    // cin >> num1;
+    // cout << "Input number 2\n";
+    // cin >> num2;
+
+    // cout << num1 * num2 << '\n';
+
+    // Find parameter of triangle
+    // int side1;
+    // int side2;
+    // int side3;
+
+    // cout << "Input side 1\n";
+    // cin >> side1;
+    // cout << "Input side 2\n";
+    // cin >> side2;
+    // cout << "Input side 1\n";
+    // cin >> side3;
+
+    // cout << "parameter of triangle " << side1 + side2 + side3 << '\n';
+
+    // Find simple interest
+    // int principle;
+    // int rate;
+    // int time;
+
+    // cout << "Entre Principle\n";
+    // cin >> principle;
+    // cout << "Entre rate\n";
+    // cin >> rate;
+    // cout << "Entre rate\n";
+    // cin >> time;
+
+    // cout << "simple interest " << (principle * rate * time) / 100 << '\n';
+
+    // Print counting n to 1
+    // int n;
+
+    // cout << "Entre any positive natural number\n";
+    // cin >> n;
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     cout << i << '\n';
+    // }
+
+    // Find factorial
+    // int n;
+
+    // cout << "Entre any positive natural number to find factorial\n";
+    // cin >> n;
+
+    // int factorial = n;
+
+    // for (int i = n - 1; i > 0; i--)
+    // {
+    //     factorial = i * factorial;
+    // }
+
+    // cout << "Factorial " << factorial << "\n";
+}
